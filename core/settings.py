0eedb94652774caa957c298c16df2bef210b444c
Datirsayali12 <sayalidatir70@gmail.com>
2024-05-16 10:06:09 +0530

    add changes

--- core/settings.py
+++ core/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/var/www/mysite/assets/'
+STATIC_ROOT = '/var/www/core/assets/'
 
 # STATIC_ROOT=""
 
